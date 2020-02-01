1c7b572696f9a204cf0258c7d29164cde78622e9
Manish Mahajan <androiddev@purpletealabs.com>
2020-02-01 13:51:01 +0530

    added user authentication token creation api

---
+++ app/user/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """ create  a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """create new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    """ renderer_classes are added to render page in browser for this api"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
